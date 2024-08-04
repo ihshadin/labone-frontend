7dcefc37f5c2a1f1d318bd8d8f620b722286c891
ihshadin <shadinfr@yahoo.com>
2024-08-04 17:34:17 +0600

    fix lot of error on the whole site

--- src/types/pagination.type.ts
+++ src/types/pagination.type.ts
@@ -2,6 +2,5 @@ import { TMeta } from "@/types/global.type";
 
 export type TPagination = {
   meta: TMeta;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   handlePaginationChange: (value: number) => void;
 };
