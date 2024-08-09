e3e1a492e5ced940123fec29c58d82585f4bb43e
OmarFaruq967 <rone967@gmail.com>
2024-08-09 19:45:24 +0600

    filter added

--- src/components/Doctors/DepartmentFilter.tsx
+++ src/components/Doctors/DepartmentFilter.tsx
@@ -1,9 +1,10 @@
 // import { useEffect, useState } from "react";
 import { Select, SelectItem } from "@nextui-org/react";
 import DepartmentList from "./DepartmentList";
+import { TbFilterPin } from "react-icons/tb";
 
 const DepartmentFilter = () => {
-//   const [department, setDepartment] = useState([]);
+  //   const [department, setDepartment] = useState([]);
 
   const departmentList = [
     { key: "Gynaecology", label: "Gynaecology" },
@@ -23,18 +24,20 @@ const DepartmentFilter = () => {
 
   //   console.log("schedules--=>", schedules);
   return (
-  <>
-    <div className="w-full py-1.5 px-2 rounded-full backdrop-filter bg-primary/10 bg-opacity-85 border-1 border-primary/30 focus:outline-0 focus:outline-primary focus:border-primary/70 duration-300 transition-all">
+    <>
+      <div className=" w-[248px] py-1.5 px-2 rounded-full backdrop-filter bg-primary/10 bg-opacity-85 border-1 border-primary/30 focus:outline-0 focus:outline-primary focus:border-primary/70 duration-300 transition-all">
         <Select
           placeholder="Filter by Department"
+          disableSelectorIconRotation
+          selectorIcon={
+            <TbFilterPin className="-translate-y-1/2 p-3 bg-primary !text-white rounded-ful" /> 
+          }
           classNames={{
             label:
               "group-data-[filled-within=true]:-translate-y-[calc(100%_+_theme(fontSize.small)/2_+_15px)] text-accent",
             trigger:
               "border rounded-full bg-primary/10 bg-opacity-85 text-accent hover:border-primary/50 data-[hover=true]:bg-white group-data-[focus=true]:bg-white group-data-[focus=true]:border-primary/50",
           }}
-          //   defaultSelectedKeys={[defaultDoctor]}
-          //   onChange={(e) => handleDoctorChange(e.target.value)}
         >
           {departmentList?.map((department) => (
             <SelectItem key={department?.key} value={department?.key}>
@@ -47,7 +50,7 @@ const DepartmentFilter = () => {
       <div className="mt-4 md:mt-5">
         <DepartmentList />
       </div>
-  </>
+    </>
   );
 };
 
