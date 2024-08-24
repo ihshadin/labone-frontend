b5389d9c15215a8cdb6b680419ba543880c93e9b
jahid <jahidmorol2@gmail.com>
2024-08-25 00:48:51 +0600

    some changes in gallery

--- src/components/Marquee/NoticeMarquee.tsx
+++ src/components/Marquee/NoticeMarquee.tsx
@@ -8,7 +8,7 @@ const NoticeMarquee = async () => {
 
   return (
     <div className="flex flex-nowrap items-center font-tiroBangla max-w-[1250px] mx-auto ">
-      <span className="h-full hidden w-[160px] bg-gradient-to-r from-[#0a8848] to-[#02a351] md:flex flex-col justify-center px-2 md:px-5 py-2 text-white text-sm md:text-lg font-medium">
+      <span className="h-full hidden w-[120px] bg-gradient-to-r from-[#0a8848] to-[#02a351] md:flex flex-col justify-center px-2 md:px-5 py-2 text-white text-sm md:text-lg font-medium">
         সেবা সমূহ
       </span>
       <Marquee speed={100} className="text-lg font-medium text-secondary">
